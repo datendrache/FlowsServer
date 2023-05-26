d89321e3b776068a63293397f1097412f327083f
Eric Knight <datendrache@hotmail.com>
2023-05-25 18:35:59 -0600

    Update to Fatum 1.0.2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿using FatumCore;
-using PhlozCore;
+﻿//   Flows Server -- Console Application Version of the Flows Server
+//
+//   Copyright (C) 2003-2023 Eric Knight
+//   This software is distributed under the GNU Public v3 License
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
 
-namespace PhlozServerDebug
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using Proliferation.Flows.Microkernel;
+
+namespace Proliferation.Flows.Server
 {
     static class Program
     {
@@ -11,15 +28,8 @@ namespace PhlozServerDebug
         //[STAThread]
         static void Main()
         {
-            try
-            {
-                var MSP = new MasterStreamProcessor(new fatumconfig());
-                MSP.Start();
-            }
-            catch (System.Threading.ThreadAbortException)
-            {
-
-            }
+            var MSP = new MasterStreamProcessor(new fatumconfig());
+            MSP.Start();
         }
     }
 }
